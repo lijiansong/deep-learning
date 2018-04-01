213f9b2839b240d91fa16297bbd9bb3e801b8aed
lijiansong <lijiansong@ict.ac.cn>
2018-04-01 12:15:22 +0800

    xxx

--- compiler/dynamic-link/tests/dylib/foo.c
+++ compiler/dynamic-link/tests/dylib/foo.c
@@ -1,10 +1,10 @@
+#include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dlfcn.h>
 int main() {
-    void* handle = dlopen("libadd.dylib", RTLD_LAZY);
-    int* addr = dlsym(handle, "add");
-    printf("address of \"add\": 0x%x\n", addr);
-    dlclose(handle);
-    return 0;
+  void *handle = dlopen("libadd.dylib", RTLD_LAZY);
+  int *addr = dlsym(handle, "add");
+  printf("address of \"add\": 0x%x\n", addr);
+  dlclose(handle);
+  return 0;
 }
