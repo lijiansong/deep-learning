1a2dbc967ebbfd8022577b3e3118900b52024822
lijiansong <lijiansong@ict.ac.cn>
2018-04-01 12:10:54 +0800

    taco foo

---
+++ compiler/dynamic-link/add.c
@@ -0,0 +1,2 @@
+int add(int a, int b) { return a + b; }
+int mul(int a, int b) { return a * b; }
---
+++ compiler/dynamic-link/tests/add/addtest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int a = 10;
+    int b = 10;
+    printf("%d\n", add(a, b));
+    return 0;
+}
